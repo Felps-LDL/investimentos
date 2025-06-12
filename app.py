b20a3b21250f39b3d09f537829bfa021fced6761
Felps_LDL <felipe.lages.lima@gmail.com>
2025-06-12 10:17:15 -0300

    Adicionando lucro na tabela e gráfico comparativo de caso a pessoa não tivesse investido

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ patrimonio = st.number_input("📈 Patrimônio atual (R$)", min_value=0.0, step=
 salario = st.number_input("💼 Seu salário atual (R$)", min_value=0.0, step=100.0)
 projecao = st.slider("⏳ Meses para projeção", 1, 240, 60)
 
+sem_investimento = 0
 investimento = patrimonio
 aumento = 12
 x, y = [], []
+a, b = [], []
+lucros = []
 
 for i in range(projecao):
     if i > aumento:
@@ -21,13 +24,20 @@ for i in range(projecao):
         aumento += 12
 
     investimento += salario * 0.3
-    lucro = investimento * 0.012
-    investimento += lucro
-
+    
     x.append(i + 1)
     y.append(investimento)
+    a.append(i + 1)
+    b.append(sem_investimento)
 
-df = pd.DataFrame({'Mês': x, 'Investimento': y})
+    lucro = investimento * 0.01
+    investimento += lucro
+    sem_investimento += salario * 0.3
+
+    lucros.append(lucro)
+
+df = pd.DataFrame({'Mês': x, 'Investimento': y, 'Lucro': lucros})
+dg = pd.DataFrame({'Mês': a, 'Aporte Mensal': b})
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=df['Mês'], y=df['Investimento'],
@@ -35,12 +45,25 @@ fig.add_trace(go.Scatter(x=df['Mês'], y=df['Investimento'],
                          name='Investimento',
                          line=dict(color='lime', width=3),
                          marker=dict(size=6)))
+
+fig.add_trace(go.Scatter(
+    x=dg['Mês'], y=dg['Aporte Mensal'],
+    mode='lines+markers',
+    name='Aporte Mensal',
+    line=dict(color='orange', width=2, dash='dot'),
+    marker=dict(size=5)
+))
+
 fig.add_hline(y=desejado, line_dash="dash", line_color="red",
               annotation_text=f"Meta: R${desejado:,.2f}",
               annotation_position="top right")
 
 fig.add_annotation(x=df['Mês'].iloc[-1], y=df['Investimento'].iloc[-1],
                    text=f"R$ {df['Investimento'].iloc[-1]:,.2f}",
+                   showarrow=True, arrowhead=1)
+
+fig.add_annotation(x=dg['Mês'].iloc[-1], y=dg['Aporte Mensal'].iloc[-1],
+                   text=f"R$ {dg['Aporte Mensal'].iloc[-1]:,.2f}",
                    showarrow=True, arrowhead=1)
 
 fig.update_layout(title='Projeção de Investimento',
@@ -53,7 +76,7 @@ st.plotly_chart(fig, use_container_width=True)
 
 st.success(f"📈 Patrimônio acumulado: R$ {investimento:,.2f}")
 
-st.dataframe(df.style.format({"Investimento": "R$ {:,.2f}"}))
+st.dataframe(df.style.format({"Investimento": "R$ {:,.2f}", "Lucro": "R$ {:,.2f}"}))
 
 st.download_button("📥 Baixar gráfico", fig.to_image(format="png"), file_name="investimento.png")
 
